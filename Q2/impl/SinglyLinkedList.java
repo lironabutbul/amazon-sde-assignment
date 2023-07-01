ec2744d6a82981efad2f70748e714af8d955313e
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-07-01 15:16:47 +0300

    Create SinglyLinkedList.java

---
+++ Q2/impl/SinglyLinkedList.java
@@ -0,0 +1,25 @@
+package Amazon_Online_Assessment_2023;
+
+public class SinglyLinkedList {
+	
+	public SinglyLinkedListNode head;
+	public SinglyLinkedListNode tail;
+	
+	public SinglyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+	
+	public void insertNode(int nodeData) {
+		SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+		
+		if(this.head == null) {
+			this.head = node;
+		} else {
+			this.tail.next = node;
+		}
+		
+		this.tail = node;
+	}
+
+}
